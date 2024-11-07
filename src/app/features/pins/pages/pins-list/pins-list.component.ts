19c20e62045603a6d7d7b59330533b2f6a02af1b
Saksham Gupta <saksham.09.gupta@gmail.com>
2024-11-07 11:32:07 +0530

    List coponent complete

--- src/app/features/pins/pages/pins-list/pins-list.component.ts
+++ src/app/features/pins/pages/pins-list/pins-list.component.ts
@@ -1,13 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { DrawerComponent } from '../../../../shared/UI/drawer/drawer.component';
-import { AddCustomerComponent } from '../../../customers/components/add-customer/add-customer.component';
-import { AddPinComponent } from '../../components/add-pin/add-pin.component';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  ViewContainerRef,
+} from '@angular/core';
 import { GlobalDataService } from '../../../../core/services/global-data.service';
 
 @Component({
   selector: 'app-pins-list',
   standalone: true,
-  imports: [DrawerComponent, AddCustomerComponent, AddPinComponent],
+  imports: [],
   templateUrl: './pins-list.component.html',
   styleUrl: './pins-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,5 +17,25 @@ import { GlobalDataService } from '../../../../core/services/global-data.service
 export class PinsListComponent {
   private dataService = inject(GlobalDataService);
 
+  private viewContainer = inject(ViewContainerRef);
+
   protected pins = this.dataService.getPins();
+
+  protected async addPinHandler() {
+    this.viewContainer.clear();
+
+    const { AddPinComponent } = await import(
+      '../../components/add-pin/add-pin.component'
+    );
+    this.viewContainer.createComponent(AddPinComponent);
+  }
+
+  protected async addCustomerHandler() {
+    this.viewContainer.clear();
+
+    const { AddCustomerComponent } = await import(
+      '../../../customers/components/add-customer/add-customer.component'
+    );
+    this.viewContainer.createComponent(AddCustomerComponent);
+  }
 }
